bbf2e7e93e3449a5b32afc2871774c966ddb5336
SRS <stian.schikora@nav.no>
2021-07-22 11:47:07 +0200

    Validation improvements and custom labels

--- force-app/main/default/lwc/communityLanguageSelector/communityLanguageSelector.js
+++ force-app/main/default/lwc/communityLanguageSelector/communityLanguageSelector.js
@@ -1,21 +1,22 @@
 import { LightningElement, track } from 'lwc';
 import setLanguage from '@salesforce/apex/CommunityLanguageSelectorUtility.setLanguage';
-import getLanguage from '@salesforce/apex/CommunityLanguageSelectorUtility.getLanguage';
+//import getLanguage from '@salesforce/apex/CommunityLanguageSelectorUtility.getLanguage';
 //import experiencelang from '@salesforce/community/language';
-import locale from '@salesforce/i18n/lang';
+//import locale from '@salesforce/i18n/lang';
+
+import LANGUAGE_PLACEHOLDER from '@salesforce/label/c.Community_Language_Selector_Placeholder';
+import LANGUAGE_NORWEGIAN_NB from '@salesforce/label/c.Community_Language_Selector_Norwegian';
+import LANGUAGE_ENGLISH from '@salesforce/label/c.Community_Language_Selector_English';
 
 export default class CommunityLanguageSelector extends LightningElement {
-    clickhere = 'Velg Språk';
-    norwegiandescriptior = 'Norsk (Bokmål)';
-    englishdescriptior = 'English';
     @track languages = [
-        { name: 'Velg Språk', label: this.clickhere, selected: true },
-        { name: 'Norwegian', label: this.norwegiandescriptior, selected: false },
-        { name: 'English', label: this.englishdescriptior, selected: false }
+        { name: 'PLACEHOLDER', label: LANGUAGE_PLACEHOLDER, selected: true },
+        { name: 'Norwegian', label: LANGUAGE_NORWEGIAN_NB, selected: false },
+        { name: 'English', label: LANGUAGE_ENGLISH, selected: false }
     ];
 
     handlePicklist(event) {
-        if (event.detail != 'Velg Språk') {
+        if (event.detail != 'PLACEHOLDER') {
             setLanguage({ language: event.detail }).then((result) => {
                 console.log(result);
                 window.location.reload();
