403f37e2f32e0f949b8c733e16b73e5fc031ed83
Morten Mikalsen <morten.mikalsen@hotmail.com>
2021-11-23 12:58:36 +0100

    Adds new expandable

--- force-app/main/default/lwc/communityExpandablePanel/communityExpandablePanel.js
+++ force-app/main/default/lwc/communityExpandablePanel/communityExpandablePanel.js
@@ -6,7 +6,34 @@ export default class CommunityExpandablePanel extends LightningElement {
     @api body;
 
     togglevisible() {
-        console.log(this.showpanel);
         this.showpanel = !this.showpanel;
+        this.performAnimation();
+    }
+
+    get chevronClass() {
+        return (
+            'nav-frontend-chevron slds-float_right ' +
+            (this.showpanel ? 'chevron--opp' : 'chevron--ned') +
+            ' chevronboks'
+        );
+    }
+
+    // To perform animations we can't use height:auto, so we use auto to get the height
+    // and set the height to that value.
+    performAnimation() {
+        let wrapper = this.template.querySelector('.dropdown');
+        if (wrapper) {
+            let content = wrapper.children[0];
+            if (this.showpanel) {
+                content.style.height = 'auto';
+                let boundingRect = content.getBoundingClientRect();
+                content.style.height = '0px';
+                window.requestAnimationFrame(function () {
+                    content.style.height = boundingRect.height + 'px';
+                });
+            } else {
+                content.style.height = '0px';
+            }
+        }
     }
 }
