da2232c6b620bcdaffbc751a3d632f49d9e9714a
Morten Mikalsen <morten.mikalsen@hotmail.com>
2022-01-28 09:45:16 +0100

    Better design for related conversation note

--- force-app/main/default/lwc/communityConversationNoteRelatedItem/communityConversationNoteRelatedItem.js
+++ force-app/main/default/lwc/communityConversationNoteRelatedItem/communityConversationNoteRelatedItem.js
@@ -19,14 +19,21 @@ export default class CommunityConversationNoteRelatedItem extends NavigationMixi
     }
 
     get date() {
-        return this.note.CRM_Date_Registered__c
-            ? ' - ' + this.note.CRM_Date_Registered__c.split('-').reverse().join('.')
-            : '';
+        if (this.note.CRM_Date_Time_Registered__c) {
+            const inputs = this.note.CRM_Date_Time_Registered__c.split('T');
+            const dateTable = inputs[0].split('-');
+            const timeTable = inputs[1].split(':');
+
+            const fullDate = new Date(Date.UTC(dateTable[0], dateTable[1], dateTable[2], timeTable[0], timeTable[1]));
+            return ' - ' + new Intl.DateTimeFormat('no-no', { dateStyle: 'long', timeStyle: 'short' }).format(fullDate);
+        } else {
+            return '';
+        }
     }
 
     get label() {
         return (
-            (this.note.CRM_Theme__r.Name ? this.note.CRM_Theme__r.Name + ' - ' : '') +
+            (this.note.CRM_Theme__r?.Name ? this.note.CRM_Theme__r.Name + ' - ' : '') +
             'Klikk her for å se relaterte samtalereferat på samme sak' +
             this.date
         );
