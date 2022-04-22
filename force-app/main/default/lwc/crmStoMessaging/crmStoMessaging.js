c7e12d4cd770e27ebcc4f415ea6cff1af38be026
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2022-04-22 11:03:29 +0200

    Oppdatert STO melding signatur - NKS-1134 Hensikten er at signaturen skal inneholde veileders faktiske enhet for å dekke de tilfellene hvor det ikke er noen fra NAV Kontaktsenter som svarer.
    crmStoMessaging.js
    * La til tom string på signaturparametrende sånn at man unngår undefined dersom tomt.
    * La til henting av enhtetsnavn basert på Department feltet på running user.
    * Utbedret get textTemplate() og la til det dynamiske enhetsnavnet
    
    STO_RecordInfoController.cls
    * La til en ny metode for å hente enhetsnavn fra enhetsnummer + tester
    
    Test data:
    * La til en test enhet under dummy-data\data
    
    Annet:
    * Fjernet en extension som ikke lenger brukes fra recommended

--- force-app/main/default/lwc/crmStoMessaging/crmStoMessaging.js
+++ force-app/main/default/lwc/crmStoMessaging/crmStoMessaging.js
@@ -2,7 +2,9 @@ import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import getRelatedRecord from '@salesforce/apex/STO_RecordInfoController.getRelatedRecord';
 import getThreadId from '@salesforce/apex/STO_RecordInfoController.getThreadIdByApiReference';
+import getUnitName from '@salesforce/apex/STO_RecordInfoController.getUnitNameFromUnitNumber';
 import NKS_FULL_NAME from '@salesforce/schema/User.NKS_FullName__c';
+import USER_DEPARTMENT from '@salesforce/schema/User.Department';
 import FIRST_NAME from '@salesforce/schema/Person__c.INT_FirstName__c';
 import CASE_THREAD_API_REFERENCE from '@salesforce/schema/Case.NKS_Henvendelse_BehandlingsId__c';
 import userId from '@salesforce/user/Id';
@@ -18,8 +20,9 @@ export default class CrmStoMessaging extends LightningElement {
     accountId;
     userId;
     personId;
-    userName;
-    supervisorName;
+    userName = '';
+    supervisorName = '';
+    supervisorDepartment = '';
     accountApiName;
     threadId;
     englishTextTemplate = false;
@@ -78,15 +81,15 @@ export default class CrmStoMessaging extends LightningElement {
     }
 
     get textTemplate() {
+        let salutation = 'Hei';
+        let regards = 'Med vennlig hilsen';
+
         if (this.englishTextTemplate == true) {
-            let greeting = '';
-            greeting = this.userName == null ? 'Hi,' : 'Hi ' + this.userName + ',';
-            return greeting + '\n\n\nKind regards\n' + this.supervisorName + '\nNAV Contact center';
-        } 
-        let greeting = '';
-        greeting = this.userName == null ? 'Hei,' : 'Hei ' + this.userName + ',';
-        return greeting + '\n\n\nMed vennlig hilsen\n' + this.supervisorName + '\nNAV Kontaktsenter';
-        
+            salutation = 'Hi';
+            regards = 'Kind regards';
+        }
+
+        return `${salutation}, ${this.userName},\n\n\n${regards}\n${this.supervisorName}\n${this.supervisorDepartment}`;
     }
 
     get threadReference() {
@@ -190,13 +193,19 @@ export default class CrmStoMessaging extends LightningElement {
 
     @wire(getRecord, {
         recordId: '$userId',
-        fields: [NKS_FULL_NAME]
+        fields: [NKS_FULL_NAME, USER_DEPARTMENT]
     })
     wiredUser({ error, data }) {
         if (error) {
             console.log(error);
         } else if (data) {
             this.supervisorName = getFieldValue(data, NKS_FULL_NAME);
+
+            getUnitName({ unitNumber: getFieldValue(data, USER_DEPARTMENT) })
+                .then((response) => (this.supervisorDepartment = response))
+                .catch((error) => {
+                    console.log(error);
+                });
         }
     }
 
