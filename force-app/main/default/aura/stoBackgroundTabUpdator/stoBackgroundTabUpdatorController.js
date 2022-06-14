ef412cdfa15d4d1e19b8183372e38a10af7939e2
Eugenijus <eugenijus.margalikas@nav.no>
2022-06-14 10:52:54 +0200

    open related cases in subtabs

--- force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
+++ force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
@@ -44,6 +44,7 @@
     },
 
     doInit: function (component, event, helper) {
+        console.log("debug 9");
         var omniAPI = component.find('omniToolkit');
         var action = component.get('c.getOnlineId');
 
@@ -64,5 +65,106 @@
             }
         });
         $A.enqueueAction(action);
-    }
+    },
+
+    onWorkAccepted : function(component, event, helper) {
+        console.log("Work accepted.");
+        var workItemId = event.getParam('workItemId');
+        var workId = event.getParam('workId');
+        console.log(workItemId);
+        console.log(workId);
+        console.log("Hello from utility bar background!");
+        var flow = component.find("ChangeOwner");
+        console.log("flow should start");
+        flow.startFlow("STO_Case_Set_Owner",
+            [
+                {
+                    name : "WorkItemId",
+                    type : "String",
+                    value: workItemId
+                },
+                {
+                    name : "WorkId",
+                    type : "String",
+                    value: workId
+                }
+            ]
+        );
+        console.log("flow started");
+    },
+    
+    handleStatusChange : function (component, event) {
+        console.log("flow status changed");
+        console.log(event.getParam("status"));
+        if(event.getParam("status") === "FINISHED_SCREEN") {
+           // Get the output variables and iterate over them
+           var outputVariables = event.getParam("outputVariables");
+           console.log("flow output");
+           console.log(outputVariables);
+           var outputVar;
+           var parentId;
+           var childIds;
+           for(var i = 0; i < outputVariables.length; i++) {
+                outputVar = outputVariables[i];
+                // Pass the values to the component's attributes
+                if(outputVar.name === "CasesToUpdate") {
+                    console.log("outputVar.value");
+                    console.log(outputVar.value);
+                    console.log(outputVar.value[0].Id);
+                    childIds = outputVar.value.map(e => e.Id);
+                    console.log(childIds);
+                } else if(outputVar.name === "WorkItemId"){
+                    parentId = outputVar.value;
+                } else {
+                }
+            }
+            if(parentId && childIds.length > 0){
+                let workspaceAPI = component.find("workspace");
+                console.log("openTabs");
+                workspaceAPI
+                    .getAllTabInfo()
+                    .then(
+                        (tabsInfo) => {
+                            console.log("tabsInfo:");
+                            console.log(tabsInfo);
+                            console.log("parentId");
+                            console.log(parentId);
+                            var tabInfo = tabsInfo
+                                .find(
+                                    (tabInfo) => {
+                                        return tabInfo.recordId ? tabInfo.recordId.startsWith(parentId) : false;
+                                    }
+                                );
+                            console.log("tabInfo");
+                            console.log(tabInfo);
+                            var parentTabId = tabInfo ? tabInfo.tabId : null;
+                            console.log("parentTabId");
+                            console.log(parentTabId);
+                            childIds
+                                .forEach(
+                                    (childId) => {
+                                        console.log("childId");
+                                        console.log(childId);
+                                        workspaceAPI
+                                            .openSubtab(
+                                                {
+                                                    tabId: parentTabId,
+                                                    recordId: childId
+                                                }
+                                            )
+                                        ;
+                                    }
+                                )
+                            ;
+                        }
+                    )
+                    .catch(
+                        (error) => {
+                            console.log(JSON.stringify(error, null, 2));
+                        }
+                    )
+                ;
+            }
+        }
+    },
 });
