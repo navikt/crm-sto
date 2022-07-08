fd579ec7106b9974120da8a1ade273719761fe7a
Eugenijus <eugenijus.margalikas@nav.no>
2022-07-08 12:35:51 +0200

    cleanup

--- force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
+++ force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
@@ -18,6 +18,7 @@
                     });
             }
         });
+
         workspace
             .getTabInfo({
                 tabId: newTabId
@@ -30,6 +31,11 @@
                         workspace.setTabLabel({
                             tabId: newTabId,
                             label: data.getReturnValue()
+                        });
+                        workspace.setTabIcon({
+                            tabId: newTabId,
+                            icon: response.icon,
+                            iconAlt: ' '
                         });
                     }
                 });
@@ -58,82 +64,5 @@
             }
         });
         $A.enqueueAction(action);
-    }/*,
-
-    onWorkAccepted : function(component, event, helper) {
-        var workItemId = event.getParam('workItemId');
-        var workId = event.getParam('workId');
-        var flow = component.find("ChangeOwner");
-        flow.startFlow("STO_Case_Set_Owner",
-            [
-                {
-                    name : "WorkItemId",
-                    type : "String",
-                    value: workItemId
-                }
-            ]
-        );
-    },
-
-    handleStatusChange : function (component, event) {
-        if(event.getParam("status") === "FINISHED_SCREEN") {
-           // Get flow response
-           var outputVariables = event.getParam("outputVariables");
-           var outputVar;
-           var parentId; // var parentId = outputVariables.find(e => e.name === "WorkItemId").value;
-           var childIds; // var childIds = outputVariables.find(e => e.name === "CasesToUpdate").value.map(e => e.Id);
-           for(var i = 0; i < outputVariables.length; i++) {
-                outputVar = outputVariables[i];
-                if(outputVar.name === "CasesToUpdate") {
-                    if(!outputVar.value) return;
-                    childIds = outputVar.value.map(e => e.Id);
-                } else if(outputVar.name === "WorkItemId"){
-                    parentId = outputVar.value;
-                } else if(outputVar.name === "ErrorMessage"){
-                    if(outputVar.value){
-                        console.log(outputVar.value);
-                        return;
-                    }
-                } else {
-                    // skip
-                }
-            }
-            if(parentId && childIds.length > 0){
-                let workspaceAPI = component.find("workspace");
-                workspaceAPI
-                    .getAllTabInfo()
-                    .then(
-                        (tabsInfo) => {
-                            var tabInfo = tabsInfo
-                                .find(
-                                    (tabInfo) => {
-                                        return tabInfo.recordId ? tabInfo.recordId.startsWith(parentId) : false;
-                                    }
-                                );
-                            var parentTabId = tabInfo ? tabInfo.tabId : null;
-                            childIds
-                                .forEach(
-                                    (childId) => {
-                                        workspaceAPI
-                                            .openSubtab(
-                                                {
-                                                    tabId: parentTabId,
-                                                    recordId: childId
-                                                }
-                                            )
-                                        ;
-                                    }
-                                )
-                            ;
-                        }
-                    )
-                    .catch(
-                        (error) => {
-                            console.log(JSON.stringify(error, null, 2));
-                        }
-                    )
-                ;
-            }
-        }
-    }*/
+    }
 });
