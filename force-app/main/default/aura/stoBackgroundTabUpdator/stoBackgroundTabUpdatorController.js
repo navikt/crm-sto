e6b121a8f3981bd38319b93689e426667fe3aa3e
SRS <stian.schikora@nav.no>
2022-07-17 20:40:55 +0200

    Removed automatic omni login

--- force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
+++ force-app/main/default/aura/stoBackgroundTabUpdator/stoBackgroundTabUpdatorController.js
@@ -41,28 +41,5 @@
                 });
                 $A.enqueueAction(action);
             });
-    },
-
-    doInit: function (component, event, helper) {
-        var omniAPI = component.find('omniToolkit');
-        var action = component.get('c.getOnlineId');
-
-        action.setCallback(this, function (data) {
-            console.log(data.getReturnValue());
-            if (data.getReturnValue() != null && data.getReturnValue().length > 0) {
-                console.log('IN Polling');
-                var poll = setInterval(function () {
-                    omniAPI
-                        .login({ statusId: data.getReturnValue() })
-                        .then(function (result) {
-                            clearInterval(poll);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                }, 2000);
-            }
-        });
-        $A.enqueueAction(action);
     }
 });
