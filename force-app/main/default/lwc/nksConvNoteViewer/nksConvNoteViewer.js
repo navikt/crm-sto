71fa70ee459c65098576a926cfdb4cc6b25ad8f1
SRS <stian.schikora@nav.no>
2022-07-22 22:00:03 +0200

    Dialogue view component

---
+++ force-app/main/default/lwc/nksConvNoteViewer/nksConvNoteViewer.js
@@ -0,0 +1,39 @@
+import { LightningElement, api, wire } from 'lwc';
+import getconvnote from '@salesforce/apex/NKS_DialogueViewController.getConvNote';
+
+export default class DiaConvNoteViewer extends LightningElement {
+    @api recordId;
+
+    conversationNote;
+    error = false;
+
+    @wire(getconvnote, { convNoteId: '$recordId' }) //Calls apex and gets conversation note record
+    wireNotes(result) {
+        if (result.error) {
+            this.error = true;
+            console.log('Error: ' + JSON.stringify(error, null, 2));
+        } else if (result.data) {
+            this.conversationNote = result.data[0];
+            console.log(JSON.stringify(this.conversationNote, null, 2));
+        }
+    }
+
+    get isLoading() {
+        return !this.conversationNote && this.error === false;
+    }
+
+    get convNoteTheme() {
+        return this.conversationNote ? this.resolve('CRM_Theme__r.Name', this.conversationNote) : '';
+    }
+
+    /**
+     * Retrieves the value from the given object's data path
+     * @param {data path} path
+     * @param {JS object} obj
+     */
+    resolve(path, obj) {
+        return path.split('.').reduce(function (prev, curr) {
+            return prev ? prev[curr] : null;
+        }, obj || self);
+    }
+}
