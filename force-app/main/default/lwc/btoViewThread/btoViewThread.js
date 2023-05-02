f7d1151efebf5b9870317d7127ab47e2ff5a31eb
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-05-02 16:33:27 +0200

    Adds thread redirect

--- force-app/main/default/lwc/btoViewThread/btoViewThread.js
+++ force-app/main/default/lwc/btoViewThread/btoViewThread.js
@@ -1,12 +1,40 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import THREAD_TYPE from '@salesforce/schema/Thread__c.CRM_Thread_Type__c';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import basepath from '@salesforce/community/basePath';
+
+// Er det bedre å ha dette harkodet her, eller gjøre det til custom metadata? Muligens noe annet?
+const urlMap = {
+    STO: (recordId) => basepath + '/skriv-til-oss/' + recordId,
+    BTO: (recordId) => basepath + '/beskjed-til-oss/visning?samtale=' + recordId
+};
 
 export default class BtoViewThread extends LightningElement {
-    threadId;
+    @api threadType;
+    @api recordId;
+    @api maxLength;
+
     @wire(CurrentPageReference)
     getStateParameters(currentPageReference) {
-        if (currentPageReference) {
-            this.threadId = currentPageReference.state.samtale;
+        if (currentPageReference && currentPageReference?.state?.samtale != null) {
+            this.recordId = currentPageReference.state.samtale;
         }
+    }
+
+    getField;
+    @wire(getRecord, { recordId: '$recordId', fields: [THREAD_TYPE] })
+    wiredThread({ error, data }) {
+        if (error) {
+            console.log('Error:', error);
+        } else if (data) {
+            const actualThreadType = getFieldValue(data, THREAD_TYPE);
+            if (this.threadType !== actualThreadType) this.redirect(actualThreadType);
+        }
+    }
+
+    redirect(actualThreadType) {
+        const link = urlMap[actualThreadType](this.recordId);
+        window.open(link, '_self');
     }
 }
