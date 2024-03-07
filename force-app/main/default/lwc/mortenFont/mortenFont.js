47dec1aefe27cb55b986a5002732c55dde57bbdc
Morten Mikalsen <morten.mikalsen@hotmail.com>
2024-03-07 09:55:05 +0100

    Rewrite solution to fix chat visual bug

--- force-app/main/default/lwc/mortenFont/mortenFont.js
+++ force-app/main/default/lwc/mortenFont/mortenFont.js
@@ -1,10 +1,11 @@
 import { LightningElement } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
-import fonter from '@salesforce/resourceUrl/fonter';
+// import fonter from '@salesforce/resourceUrl/fonter';
+import mbyFix from '@salesforce/resourceUrl/mbyFix';
 
 export default class MortenFont extends LightningElement {
     connectedCallback() {
-        loadStyle(this, fonter);
+        loadStyle(this, mbyFix);
     }
 }
