dfd5453c4dc5d379982be2df79dd3e90da30aeee
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-06-18 09:31:56 +0200

    added notifications to sto

--- force-app/main/default/lwc/stoMessagingContainer/stoMessagingContainer.js
+++ force-app/main/default/lwc/stoMessagingContainer/stoMessagingContainer.js
@@ -12,6 +12,7 @@ import JOURNAL_LABEL from '@salesforce/label/c.NKS_Journal';
 import CREATE_NAV_TASK_LABEL from '@salesforce/label/c.NKS_Create_NAV_Task';
 import SET_TO_REDACTION_LABEL from '@salesforce/label/c.NKS_Set_To_Redaction';
 import { publishToAmplitude } from 'c/amplitude';
+import { handleShowNotifications } from 'c/nksButtonContainerUtils';
 
 const CONSTANTS = {
     CREATE_NAV_TASK: 'createNavTask',
@@ -60,82 +61,6 @@ export default class StoMessagingContainer extends LightningElement {
 
     connectedCallback() {
         this.initializeCaseId();
-    }
-
-    @wire(getRecord, { recordId: '$caseId', fields: [STATUS_FIELD, IN_QUEUE_FIELD] })
-    wiredRecord(result) {
-        this.wiredCase = result;
-        const { data, error } = result;
-        if (data) {
-            this.status = getFieldValue(data, STATUS_FIELD);
-            this.inQueue = getFieldValue(data, IN_QUEUE_FIELD);
-        } else if (error) {
-            console.error(error.body.message);
-        }
-    }
-
-    initializeCaseId() {
-        if (this.isThread) {
-            getRelatedRecord({
-                parentId: this.recordId,
-                relationshipField: CONSTANTS.CASE_FIELD_API_NAME,
-                objectApiName: this.objectApiName
-            })
-                .then((record) => {
-                    this.caseId = this.resolve(CONSTANTS.CASE_FIELD_API_NAME, record);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        } else {
-            this.caseId = this.recordId;
-        }
-    }
-
-    toggleButton(buttonName, event) {
-        this.label = event.target?.label;
-        const buttons = ['Reserve', 'PutBack', 'Transfer', 'Journal', 'CreateNavTask', 'Redact'];
-        buttons.forEach((button) => {
-            this[`show${button}`] = button === buttonName ? !this[`show${button}`] : false;
-        });
-    }
-
-    resetButtonVisibility() {
-        this.showReserve = false;
-        this.showPutBack = false;
-        this.showTransfer = false;
-        this.showRedact = false;
-        this.showJournal = false;
-        this.showCreateNavTask = false;
-    }
-
-    handleFlowStatusChange(event) {
-        const flowStatus = event.detail.status;
-        if (flowStatus === CONSTANTS.FINISHED || flowStatus === CONSTANTS.FINISHED_SCREEN) {
-            refreshApex(this.wiredCase);
-            this.resetButtonVisibility();
-            publishToAmplitude('STO', { type: `${this.label} pressed` });
-        }
-    }
-
-    handleSubmit() {
-        if (!this.completeDisabled) {
-            this.resetButtonVisibility();
-            this.showComplete = !this.showComplete;
-        }
-    }
-
-    handleSubmitStatusChange(event) {
-        const flowStatus = event.detail.status;
-        if (flowStatus === CONSTANTS.FINISHED || flowStatus === CONSTANTS.FINISHED_SCREEN) {
-            refreshApex(this.wiredCase);
-            this.showComplete = false;
-            publishToAmplitude('STO', { type: 'Complete/Send pressed' });
-        }
-    }
-
-    resolve(path, obj) {
-        return path.split('.').reduce((prev, curr) => (prev ? prev[curr] : null), obj);
     }
 
     get inputVariables() {
@@ -259,5 +184,87 @@ export default class StoMessagingContainer extends LightningElement {
 
     get transferDisabled() {
         return !this.inQueue;
+    }
+
+    get notificationBoxTemplate() {
+        return this.template.querySelector('c-nks-notification-box');
+    }
+
+    @wire(getRecord, { recordId: '$caseId', fields: [STATUS_FIELD, IN_QUEUE_FIELD] })
+    wiredRecord(result) {
+        this.wiredCase = result;
+        const { data, error } = result;
+        if (data) {
+            this.status = getFieldValue(data, STATUS_FIELD);
+            this.inQueue = getFieldValue(data, IN_QUEUE_FIELD);
+        } else if (error) {
+            console.error(error.body.message);
+        }
+    }
+
+    initializeCaseId() {
+        if (this.isThread) {
+            getRelatedRecord({
+                parentId: this.recordId,
+                relationshipField: CONSTANTS.CASE_FIELD_API_NAME,
+                objectApiName: this.objectApiName
+            })
+                .then((record) => {
+                    this.caseId = this.resolve(CONSTANTS.CASE_FIELD_API_NAME, record);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        } else {
+            this.caseId = this.recordId;
+        }
+    }
+
+    toggleButton(buttonName, event) {
+        this.label = event.target?.label;
+        const buttons = ['Reserve', 'PutBack', 'Transfer', 'Journal', 'CreateNavTask', 'Redact'];
+        buttons.forEach((button) => {
+            this[`show${button}`] = button === buttonName ? !this[`show${button}`] : false;
+        });
+    }
+
+    resetButtonVisibility() {
+        this.showReserve = false;
+        this.showPutBack = false;
+        this.showTransfer = false;
+        this.showRedact = false;
+        this.showJournal = false;
+        this.showCreateNavTask = false;
+    }
+
+    handleFlowStatusChange(event) {
+        const { status, outputVariables, flowTitle } = event.detail;
+
+        if (status === CONSTANTS.FINISHED || status === CONSTANTS.FINISHED_SCREEN) {
+            refreshApex(this.wiredCase);
+            this.resetButtonVisibility();
+            publishToAmplitude('STO', { type: `${this.label} pressed` });
+            handleShowNotifications(flowTitle, outputVariables, this.notificationBoxTemplate);
+        }
+    }
+
+    handleSubmit() {
+        if (!this.completeDisabled) {
+            this.resetButtonVisibility();
+            this.showComplete = !this.showComplete;
+        }
+    }
+
+    handleSubmitStatusChange(event) {
+        const flowStatus = event.detail.status;
+        if (flowStatus === CONSTANTS.FINISHED || flowStatus === CONSTANTS.FINISHED_SCREEN) {
+            refreshApex(this.wiredCase);
+            this.showComplete = false;
+            publishToAmplitude('STO', { type: 'Complete/Send pressed' });
+        }
+    }
+
+    resolve(path, obj) {
+        return path.split('.').reduce((prev, curr) => (prev ? prev[curr] : null), obj);
     }
 }
