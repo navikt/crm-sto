0d3cd42e9213102bd45104361eeab23e4c1da170
Jan Robert Gjøstøl <jan.robert.gjostol@nav.no>
2025-05-28 13:48:15 +0200

    added 'utskrift'-button

--- force-app/main/default/lwc/threadExpandedTimeline/threadExpandedTimeline.js
+++ force-app/main/default/lwc/threadExpandedTimeline/threadExpandedTimeline.js
@@ -57,4 +57,21 @@ export default class ThreadExpandedTimeline extends NavigationMixin(LightningEle
             publishToAmplitude('Timeline', { type: 'Navigate to record' });
         }
     }
+
+    handlePrint() {
+        console.log(this.recordId);
+        if (!this.recordId) {
+            console.error('No record ID found!');
+            return;
+        }
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__webPage',
+            attributes: {
+                url: `/apex/NKS_RecordPrint?id=${this.recordId}`
+            }
+        }).then((url) => {
+            console.log('Opening URL:', url);
+            window.open(url, '_self');
+        });
+    }
 }
