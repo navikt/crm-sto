179ffeb4b82a5ced06e6bae032fc56ce2292bff8
eirikfladby <eirik.fladby@gmail.com>
2026-05-20 13:39:35 +0200

    show user name as clickable link to account on oppgave page

--- force-app/main/default/aura/crmOppgaveNavigation/crmOppgaveNavigationController.js
+++ force-app/main/default/aura/crmOppgaveNavigation/crmOppgaveNavigationController.js
@@ -4,7 +4,10 @@
         if (!pageRef || !pageRef.state) return;
         const oppgaveId = pageRef.state.c__oppgaveId;
         const oppgavetype = pageRef.state.c__oppgavetype;
+        const fromListView =
+            pageRef.state.c__fromOppgaveListHome === true || pageRef.state.c__fromOppgaveListHome === 'true';
         component.find('oppgavePage').set('v.oppgaveId', oppgaveId);
+        component.find('oppgavePage').set('v.showPersonInfo', fromListView);
         const workspace = component.find('workspace');
         workspace.getEnclosingTabId().then(function (tabId) {
             workspace.setTabLabel({ tabId: tabId, label: oppgavetype });
