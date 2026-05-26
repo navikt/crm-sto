0c18bd696c7b740b5dbf6e936b86b32caff26204
Morten Mikalsen <morten.mikalsen@hotmail.com>
2026-05-26 14:52:07 +0200

    Reenable Bob reporting Modal (with feature toggle)

--- force-app/main/default/aura/nksAmplitudeUtility/nksAmplitudeUtilityController.js
+++ force-app/main/default/aura/nksAmplitudeUtility/nksAmplitudeUtilityController.js
@@ -1,3 +1,17 @@
 ({
-    myAction: function () {}
+    getBobFeature: function (component) {
+        var action = component.get('c.isFeatureEnabled');
+        action.setParams({ featureName: 'Bob_Reporting_Data', toogleType: 'FEATURE_FLAG' });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                console.log('Feature flag check successful: ' + response.getReturnValue());
+                var isEnabled = response.getReturnValue();
+                component.set('v.isBobEnabled', isEnabled);
+            } else {
+                console.error('Error checking feature flag: ' + JSON.stringify(response.getError()));
+            }
+        });
+        $A.enqueueAction(action);
+    }
 });
