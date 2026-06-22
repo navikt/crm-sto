c7a107acc18bd26dfe8032128c0c87eb307d8964
Sara Mohammadi <86772218+saramoh2309@users.noreply.github.com>
2026-06-22 13:44:03 +0200

    use same ingress for yrkesskade and internasjonal

--- force-app/main/default/lwc/stoRegisterThread/stoRegisterThread.js
+++ force-app/main/default/lwc/stoRegisterThread/stoRegisterThread.js
@@ -37,8 +37,7 @@ import STO_HJELPEMIDLER_INGRESS from '@salesforce/label/c.Skriv_til_oss_Hjelpemi
 import BTO_DEFAULT_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Default_ingress';
 import ENDRING_DEFAULT_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Endring_Default_ingress';
 import ENDRING_PENSJON_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Endring_Pensjon_ingress';
-import ENDRING_INTERNASJONAL_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Endring_Internasjonal_ingress';
-import ENDRING_YRKESSKADE_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Endring_Yrkesskade_ingress';
+import ENDRING_INTERNASJONAL_YRKESSKADE_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Endring_Internasjonal_Yrkesskade_ingress';
 import TREKK_SOKNAD_DEFAULT_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Trekk_soknad_Default_ingress';
 import BESKJED_INTERNASJONAL_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Beskjed_Internasjonal_ingress';
 import BESKJED_DEFAULT_INGRESS from '@salesforce/label/c.Beskjed_til_oss_Beskjed_Default_ingress';
@@ -198,8 +197,8 @@ export default class StoRegisterThread extends NavigationMixin(LightningElement)
         'Meld fra om endring': {
             default: ENDRING_DEFAULT_INGRESS,
             'AFP i offentlig sektor': ENDRING_PENSJON_INGRESS,
-            'Bor eller jobber i utlandet': ENDRING_INTERNASJONAL_INGRESS,
-            Yrkesskade: ENDRING_YRKESSKADE_INGRESS
+            'Bor eller jobber i utlandet': ENDRING_INTERNASJONAL_YRKESSKADE_INGRESS,
+            Yrkesskade: ENDRING_INTERNASJONAL_YRKESSKADE_INGRESS
         },
         'Trekke en søknad': {
             default: TREKK_SOKNAD_DEFAULT_INGRESS
